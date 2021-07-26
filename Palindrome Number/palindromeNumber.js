c9dbfdeab28088ac325d9187b5edf7025473f378
John-Witter <mrsolginski@gmail.com>
2021-07-26 13:03:51 -0400

    palindromeNumber.js

---
+++ Palindrome Number/palindromeNumber.js
@@ -0,0 +1,55 @@
+// https://leetcode.com/problems/palindrome-number/
+
+/*  
+Given an integer x, return true if x is palindrome integer.
+
+An integer is a palindrome when it reads the same backward as forward. For 
+    example, 121 is palindrome while 123 is not.
+*/
+
+/*
+Example 1:
+Input: x = 121
+Output: true
+
+Example 2:
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 
+    121-. Therefore it is not a palindrome.
+
+Example 3:
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+Example 4:
+Input: x = -101
+Output: false
+*/
+
+/*
+        Constraints:
+    -231 <= x <= 231 - 1
+*/
+
+// Follow up: Could you solve it without converting the integer to a string?
+
+var isPalindrome = function(x) {
+    if (x < 0) return false
+    let xStr = String(x)
+    return xStr === xStr.split('').reverse().join('')
+}
+
+/* the pattern I've figured out so far
+
+var isPalindrome = function(121) {
+    let a = x % 10 // 1
+    let b = Math.floor(x / 10) //12
+    let c = b % 10 // 2
+    let d = Math.floor(b / 10) // 1
+
+    if (a === d) return true
+};
+
+*/
